61aaf0a6f886149570ee6494f1aba1c8925aea4a
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-02-12 03:29:48 +0900

    parallel fetch with <Suspense>

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,11 +1,22 @@
+import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 async function getMovie(id:string) {
+    console.log(`Fetching movies ${Date.now()}`);
+    await new Promise((resolve)=>setTimeout(resolve,5000));
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
 
 export default async function MovieDetail({params:{id}}:{params:{id:string}}){
-    const movie = await getMovie(id);
-    return <h1>{movie.title}</h1>;
+    return <div>
+        <Suspense fallback={<h1>Loading movie info</h1>}>
+            <MovieInfo id={id}/>
+        </Suspense>
+        <Suspense fallback={<h1>Loading movie videos</h1>}>
+            <MovieVideos id={id}/>
+        </Suspense>
+    </div>
 }
