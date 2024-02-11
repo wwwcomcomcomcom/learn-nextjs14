5ca25833f3a0e06039c93ebca9257fd5e1777fad
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-02-12 03:37:51 +0900

    Error handling page added

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,14 +1,6 @@
 import { Suspense } from "react";
-import { API_URL } from "../../../(home)/page";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
-
-async function getMovie(id:string) {
-    console.log(`Fetching movies ${Date.now()}`);
-    await new Promise((resolve)=>setTimeout(resolve,5000));
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
-}
 
 export default async function MovieDetail({params:{id}}:{params:{id:string}}){
     return <div>
